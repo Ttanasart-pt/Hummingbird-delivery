db77a839a2167389b04e3861fd5f2dd8f89de47d
MakhamDev <tanasart_pt@outlook.co.th>
2021-10-15 16:14:32 +0700

    link delivery data to database.

--- user/delivery_data.php
+++ user/delivery_data.php
@@ -1,3 +1,5 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/Hummingbird_delivery/connect.php'); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <link rel="stylesheet" href="/Hummingbird_delivery/style_master.css">
@@ -53,19 +55,43 @@
                     <th>Package type</th>
                     <th>Status</th>
                 </tr>
-
-                <tr class="data-row">
-                    <td><svg xmlns="http://www.w3.org/2000/svg" width="28.779" height="20.145" viewBox="0 0 28.779 20.145">
-                        <path id="Icon_awesome-box-open" data-name="Icon awesome-box-open" 
-                            d="M19.14,12.321a2.18,2.18,0,0,1-1.862-1.052L14.388,6.476,11.5,11.269a2.187,2.187,0,0,1-1.866,1.057,2.068,2.068,0,0,1-.6-.085l-6.16-1.763v8a1.434,1.434,0,0,0,1.088,1.394l9.721,2.433a2.923,2.923,0,0,0,1.394,0l9.73-2.433A1.442,1.442,0,0,0,25.9,18.481v-8l-6.16,1.758A2.068,2.068,0,0,1,19.14,12.321ZM28.7,7.276,26.384,2.654a.734.734,0,0,0-.751-.4L14.388,3.688l4.123,6.839a.739.739,0,0,0,.832.328l8.9-2.541A.741.741,0,0,0,28.7,7.276ZM2.391,2.654.075,7.276A.733.733,0,0,0,.53,8.31l8.9,2.541a.739.739,0,0,0,.832-.328l4.128-6.835L3.137,2.254a.735.735,0,0,0-.746.4Z" transform="translate(0.003 -2.247)"/>
-                      </svg>
-                      </td>
-                    <td>123</td>
-                    <td>123</td>
-                    <td>123</td>
-                </tr>
+                <?php
+                    $userid = 1;
+                    $q = "SELECT d.delivery_id, l.location_name, pt.name AS package_type, ds.name AS status
+                          FROM delivery d, delivery_status ds, location l, package p, package_type pt  
+                          WHERE d.sender_id = {$userid}
+                            AND d.delivery_status = ds.deli_status_id
+                            AND d.package_id = p.package_id
+                            AND p.type = pt.package_type_id
+                            AND d.destination = l.location_id;";
+                    
+                    $result = $mysqli -> query($q);
+                    if(!$result) {
+                        echo "<div class='error-box'>Query failed : {$mysqli -> error} </div>";
+                        return;
+                    }
+                    while($row = $result -> fetch_array()) { ?>
+                    <tr class="data-row">
+                        <td><svg xmlns="http://www.w3.org/2000/svg" width="28.779" height="20.145" viewBox="0 0 28.779 20.145">
+                            <path id="Icon_awesome-box-open" data-name="Icon awesome-box-open" 
+                                d="M19.14,12.321a2.18,2.18,0,0,1-1.862-1.052L14.388,6.476,11.5,11.269a2.187,2.187,0,0,1-1.866,1.057,2.068,2.068,0,0,1-.6-.085l-6.16-1.763v8a1.434,1.434,0,0,0,1.088,1.394l9.721,2.433a2.923,2.923,0,0,0,1.394,0l9.73-2.433A1.442,1.442,0,0,0,25.9,18.481v-8l-6.16,1.758A2.068,2.068,0,0,1,19.14,12.321ZM28.7,7.276,26.384,2.654a.734.734,0,0,0-.751-.4L14.388,3.688l4.123,6.839a.739.739,0,0,0,.832.328l8.9-2.541A.741.741,0,0,0,28.7,7.276ZM2.391,2.654.075,7.276A.733.733,0,0,0,.53,8.31l8.9,2.541a.739.739,0,0,0,.832-.328l4.128-6.835L3.137,2.254a.735.735,0,0,0-.746.4Z" transform="translate(0.003 -2.247)"/>
+                        </svg>
+                        </td>
+                        <td><?php echo $row['location_name'] ?></td>
+                        <td><?php echo $row['package_type'] ?></td>
+                        <td><?php echo $row['status'] ?></td>
+                    </tr>
+                <?php } ?>
             </table>
+
+            <?php
+                $count = $result -> num_rows;
+                echo "<div class='content-center'><p>Showing {$count} results.</p></div>";
+                $result -> free();
+            ?>
         </div>
     </div>
 </body>
 </html>
+
+<?php $mysqli -> close(); ?>
